cbf3cd5e6f03ddb623ce3a791b2f8b24376cb12c
david <daviidap07@gmail.com>
2025-08-07 23:12:33 -0500

    feat: add main menu and switch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TYPE AN OPTION");
+        int option = 0;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            do {
+                System.out.println("WELCOME TO CALCULATOR");
+                System.out.println("1. Addition");
+                System.out.println("2. Subtraction");
+                System.out.println("3. Multiplication");
+                System.out.println("4. Division");
+                System.out.println("5. Power");
+                System.out.println("6. Square Root");
+                System.out.println("0. Exit");
+                System.out.print("Type an option: ");
+                option = sc.nextInt();
+
+                switch (option) {
+                    case 0:
+                        System.exit(0);
+                        break;
+                    case 1:
+                        System.out.println("ADDITION");
+                        break;
+                    case 2:
+                        System.out.println("SUBTRACTION");
+                        break;
+                    case 3:
+                        System.out.println("MULTIPLICATION");
+                        break;
+                    case 4:
+                        System.out.println("DIVISION");
+                        break;
+                    case 5:
+                        System.out.println("POWER");
+                        break;
+                    case 6:
+                        System.out.println("SQUARE ROOT");
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                }
+            }
+            while (option != 7);
+        }
+        catch (Exception e) {
+
         }
     }
+}
